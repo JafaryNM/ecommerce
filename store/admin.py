559620894dc812d09238090c619222892f74599a
JafaryNm <jaffarymdegela@gmail.com>
2022-03-13 22:26:05 +0300

    Update Model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category,Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    prepopulate_field={'slug':('name',)}
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['title','slug','author','instock','price','created_date','update_date']
+    list_filter=['instock','isactive']
+    list_editable=['instock','price']
+    prepopulate_field={'slug':('title',)}
+
--- store/models.py
+++ store/models.py
@@ -1,3 +1,39 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+   name=models.CharField(max_length=200, db_index=True, null=True,blank=True )
+   slug=models.SlugField(max_length=100, unique=True)
+   
+   class  Meta:
+     verbose_name_plural = 'Categories'
+     def __str__(self):
+         return self.name
+     
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='Product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='ProductCreator', on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length = 150)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    instock=models.BooleanField(default=True)
+    isactive=models.BooleanField(default=True)
+    created_date=models.DateField(auto_now_add='True')
+    update_date=models.DateField(auto_now=True)
+    
+    class  Meta:
+        verbose_name_plural = 'Products'
+        ordering=('-created_date',)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
+    
