3fa6fc6fae717cf8835f110d4619e6ab21597857
JafaryNm <jaffarymdegela@gmail.com>
2022-03-20 13:01:03 +0300

    Update Display Product

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name='store'
 
 urlpatterns = [
     path('' , DisplayProducts.as_view(), name='viewallproducts')
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from pyexpat import model
 from typing import List
+from unicodedata import category
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import *
@@ -10,4 +11,12 @@ from .models import *
 class DisplayProducts(ListView):
     model=Product
     tempalate_name='store/home.html'
+    category=Category.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super(DisplayProducts, self).get_context_data(**kwargs)
+        context['products']=Product.objects.all()
+        context['categories']=self.category
+        return context
+    
     
